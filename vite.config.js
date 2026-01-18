bea8d85720ac7f0ea20a21bf217176e574a55662
Dávid Gálus <201229475+DavidGalus2@users.noreply.github.com>
2026-01-18 11:57:07 +0100

    Maškrty

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,17 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Frontedy/',
+  // GitHub Pages – názov repozitára
+  base: '/Frontendy/',
+
   plugins: [
     vue(),
     vueDevTools(),
   ],
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  // Pre GitHub Pages nastavte base na '/nazov-repozitara/'
-  // base: '/masky-pre-psov/',
 })
